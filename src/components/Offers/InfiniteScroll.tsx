26eae7fef5519a7a9611f7ac0133096aae7cd04f
Abhishek Thapliyal <abhishek.thapliyal@plexure.com>
2021-01-19 08:07:50 +1300

    Fix typos in the notes

--- src/components/Offers/InfiniteScroll.tsx
+++ src/components/Offers/InfiniteScroll.tsx
@@ -1,17 +1,16 @@
 /* STEPS TO WRITE REACT COMPONENT
 
-1. Import all necessary dependencies - similar to using statements on top on C# files
+1. Import all necessary dependencies - similar to using statements on top of C# files
 2. Declare a component which is just a JS function that will return your html and css later
 3. useState is a function hook provided by React that keeps track of your state changes. E.g. loading state, list of offers etc. Whenever this state changes the component will re-render
 5. useEffect is a function hook that takes two parameters - a function and an array. The first parameter function will run automatically whenever the dependencies in the array change
 6. useEffect hook is generally used to make API calls or in other words handle side-effects
 7. Write your html and css and that's it!
-8. If you understand this InfiniteScroll component you just learnt 75% of React. Rest 25% are advanced concepts
+8. If you understand this InfiniteScroll component you just learnt 75% - 80% of React. Rest 25% are advanced concepts, performance hooks and routing stuff
 */
 
 // Step 1: Import all the dependencies
 import React, { useState, useEffect } from "react";
-import { useHistory } from "react-router-dom";
 import useInfiniteScroll from "react-infinite-scroll-hook";
 import { Table, Spinner } from "react-bootstrap";
 import { getOffers, addMultipleOffersForInfiniteScroll } from "../../services/offers-service";
@@ -26,14 +25,16 @@ export const InfiniteScroll: React.FC = () => {
   // Step 3: Create your state using useState. This returs the state and the function to update it. 
   // E.g. in below loading is a boolean with initial value of false. Whenever you have to update it just use setLoading function and pass in the new value
   const [loading, setLoading] = useState<boolean>(false);
+
   // Similarly for offers to keep track of list of offers
   const [offers, setOffers] = useState<Offer[]>([]);
+
   // And page number to support infinite scroll and make the API call with correct page number
   let [currentPage, setCurrentPage] = useState<number>(1);
 
 
   // Step 4: Use useEffect hook to manage side effects
-  // Here we are firing a reques to the backend to add 20000 offers so we have enough data to show Infinite Scroll
+  // Here we are firing a request to the backend to add 20000 offers so we have enough data to show Infinite Scroll
   // We had to do this for demo purposes as we didn't have a lot of offers to showcase Infinite scroll
   useEffect(() => {
     addMultipleOffersForInfiniteScroll();
@@ -43,6 +44,7 @@ export const InfiniteScroll: React.FC = () => {
 
   // Step 4: Use useEffect to fire another call to backend to get new list of offers
   // For this useEffect our dependency array has currentPage state. This means whenever currentPage changes we run this effect again and will fetch new offers
+  // This WON'T run when first time the component is rendered and only runs on dependency change
   useEffect(() => {
     // As we are fetching new data we set loading state to true. Later this is used in html to render a spinner if loading is true
     setLoading(true);
